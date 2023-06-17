c863913c0f3f7a0c793bb736b3ad406b3356ff86
Cameron Yick <cameron.yick@datadoghq.com>
2023-06-17 11:50:04 -0400

    improve docstring for string sanitization

--- lux/utils/sanitize_column_name.py
+++ lux/utils/sanitize_column_name.py
@@ -6,9 +6,12 @@ vega_replacement_pattern = r'(?<!\\)\.'
 
 def sanitize_column_name(name):
     """
-    When vega-lite sees a period ".", it assumes it's looking at a nested object
-    Since lux doesn't process "object"/"dict" columns, this assumption is unhelpful to us
+    When vega-lite sees a period ".", it assumes it's looking at data with nested properties.
 
-    To treat "." as a regular character and not a call for nested property access, we escape it.
+    Since lux doesn't process "object"/"dict" columns, this assumption is unhelpful when trying to render the recommended graphs
+
+    To treat "." as a regular character and not a call for nested property access, we escape it with a backslash.
+
+    https://vega.github.io/vega-lite/docs/field.html
     """
     return re.sub(vega_replacement_pattern, "\\.", name)
